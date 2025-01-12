df7ce0a6a0a395b2c18741157b75b367b4a76235
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-12 09:02:56 +0500

    Update flashlight button text based on state and ensure proper positioning

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
   flashlight.classList.toggle("flash");
+  if (flashlight.classList.contains("flash")) {
+    button.textContent = "Chiroqni o'chirish";
+  } else {
+    button.textContent = "Chiroqni yoqish";
+  }
 });
 
-// flashlight.style.transform = "translate(-50%, -50%)";
+flashlight.style.transform = "translate(-50%, -50%)";
 document.body.addEventListener("mouseover", (e) => {
   flashlight.style.top = e.clientY + "px";
   flashlight.style.left = e.clientX + "px";
